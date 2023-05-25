10bd7d4caa17259a2bd4362437ec6626458213e0
Ana Verena Gomes <103127979+anavgbc@users.noreply.github.com>
2023-05-25 09:35:01 -0300

    fix: remove comments

--- hands-on/src/services/axios.js
+++ hands-on/src/services/axios.js
@@ -1,21 +1,6 @@
 // import Vue from "vue";
 import axios from "axios";
 
-// axios.defaults.headers.post["Content-Type"] =
-//   "application/x-www-form-urlencoded";
-// axios.defaults.headers.common["Authorization"] = "Bearer YOUR_ACCESS_TOKEN";
-
-// Vue.use({
-//   install(Vue) {
-//     Vue.prototype.$http = axios.create({
-//       baseURL: "https://accounts.spotify.com/api",
-//       headers: {
-//         post: { "Content-Type": "application/x-www-form-urlencoded" },
-//       },
-//     });
-//   },
-// });
-
 const api = axios.create({
   baseURL: "https://api.spotify.com",
   headers: {
@@ -28,9 +13,6 @@ export const apiToken = axios.create({
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
   },
-  //   params: {
-  //     grant_type: "authorization_code",
-  //   },
 });
 
 export default api;
